6785864341cdf2168196f8c42c54a0ab7ed2cedf
aagoldst <avi.goldstein95@gmail.com>
2016-10-05 02:10:24 -0400

    updates to css and contact entry

--- Chrome/src/browser_action/views/contacts/contact-entry.jsx
+++ Chrome/src/browser_action/views/contacts/contact-entry.jsx
@@ -5,18 +5,52 @@ var ContactEntry = React.createClass({
     name : React.PropTypes.string.isRequired
   },
 
-  render : function(){
-    return(
-      <div className= "contactEntry">
-        <div id = "contact_info">
-          <h2>{this.props.name}</h2>
-        </div>
+  handleChange: function(event) {
+    this.props.onUserInput(this.refs.searchStringInput.value);
+    this.setState({name: event.refs.name.value});
+    console.log(this.props.name);
+  },
 
-        <div className="edit_button">
-          <button type="button" className="gray-button" id="edit_contact">EDIT</button>
+  getInitialState : function(){
+    return {editable : false};
+  },
+
+  onClickEdit : function(){
+    this.setState({editable : true});
+  },
+
+  onClickDone : function(){
+    this.setState({editable : false});
+  },
+
+  render : function(){
+    if(this.state.editable){
+      return(
+        <div className= "contactEntry">
+          <div id = "contact_info">
+            <form>
+              <input type="text" ref = "name" onChange={this.handleChange} value={this.props.name} ></input>
+            </form>
+          </div>
+
+          <div id ="done_button">
+            <button type="button" className="gray-button" id="edit_contact" onClick={this.onClickDone}>DONE</button>
+          </div>
         </div>
-      </div>
+      );
+    }else{
+      return(
+        <div className= "contactEntry">
+          <div id = "contact_info">
+            <h2>{this.props.name}</h2>
+          </div>
+
+          <div id ="edit_button">
+            <button type="button" className="gray-button" id="edit_contact" onClick={this.onClickEdit}>EDIT</button>
+          </div>
+        </div>
     );
+    }
   }
 });
 
