8133ce98d5367cca7cfcbad63d1f564c961c75eb
aagoldst <avi.goldstein95@gmail.com>
2016-10-14 12:58:28 -0400

    updating to new UI

--- Chrome/src/browser_action/popup.jsx
+++ Chrome/src/browser_action/popup.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PageContainer = require('./views/page-container.jsx');
-var NewGroupPage = require('./views/new_group/new-group-page.jsx');
+var ContactsPage = require('./views/contacts-page.jsx');
 var StorageManager = require('./storage_manager.js');
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -10,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function loadPage() {
   ReactDOM.render(
-    <PageContainer storageManager={new StorageManager()}/>,
+    <ContactsPage storageManager={new StorageManager()}/>,
     document.getElementById('page')
   );
 }
